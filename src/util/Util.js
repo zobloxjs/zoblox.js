56695f9b47684c7d8c4d92c97a3f3a3b22306c34
Glitch (zobloxjs) <none>
2023-07-06 17:04:19 +0000

    🐫🎎 Updated with Glitch

--- src/util/Util.js
+++ src/util/Util.js
@@ -1,36 +1,36 @@
 const axios = require('axios'); 
- const fs = require('node:fs'); 
+const fs = require('node:fs'); 
   
- async function resolveImage(image, returnStream = false) { 
-   let imageStream; 
+async function resolveImage(image, returnStream = false) { 
+  let imageStream; 
   
-   if (/^https?:\/\//.test(image)) { 
-     const response = await axios({ 
-       method: 'GET', 
-       url: image, 
-       responseType: 'stream' 
-     }); 
-     imageStream = response.data; 
-   } else if (typeof image === 'string') { 
-     imageStream = fs.createReadStream(image); 
-   } else if (typeof image === 'object' && image instanceof Buffer) { 
-     imageStream = image; 
-     returnStream = true;
-   } else { 
-     throw new Error('Unsupported image type'); 
-   } 
+  if (/^https?:\/\//.test(image)) { 
+    const response = await axios({ 
+      method: 'GET', 
+      url: image, 
+      responseType: 'stream' 
+    }); 
+    imageStream = response.data; 
+  } else if (typeof image === 'string') { 
+    imageStream = fs.createReadStream(image); 
+  } else if (typeof image === 'object' && image instanceof Buffer) { 
+    imageStream = image; 
+    returnStream = true;
+  } else { 
+    throw new Error('Unsupported image type'); 
+  } 
   
-   if (returnStream) { 
-     return imageStream; 
-   } else { 
-     const chunks = []; 
-     for await (const chunk of imageStream) { 
-       chunks.push(chunk); 
-     } 
-     return Buffer.concat(chunks); 
-   } 
- } 
+  if (returnStream) { 
+    return imageStream; 
+  } else { 
+    const chunks = []; 
+    for await (const chunk of imageStream) { 
+      chunks.push(chunk); 
+    } 
+    return Buffer.concat(chunks); 
+  } 
+} 
   
- module.exports = { 
-   resolveImage 
- }
+module.exports = { 
+  resolveImage 
+}
